1615444d8295c5b7c490be061b78a32b2339b6d5
Ty Husband <56952190+tyhusband@users.noreply.github.com>
2025-11-19 23:07:36 -0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/Nov29-Invitation/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
